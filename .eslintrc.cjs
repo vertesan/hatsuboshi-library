d38ed4259b5f41828f7168a27985b4cd44d5db8d
vertesan <162897218+vertesan@users.noreply.github.com>
2024-06-22 15:17:59 +0000

    chore(lint): turn off `no-unused-vars`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -71,6 +71,9 @@ module.exports = {
         "plugin:import/recommended",
         "plugin:import/typescript",
       ],
+      rules: {
+        "@typescript-eslint/no-unused-vars": "off",
+      },
     },
 
     // Node
