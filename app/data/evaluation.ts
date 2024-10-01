fc04bde6f7c0c2bc2a4025445c63ea54301df4c3
vertesan <162897218+vertesan@users.noreply.github.com>
2024-10-01 16:33:20 +0000

    feat: splus

--- app/data/evaluation.ts
+++ app/data/evaluation.ts
@@ -4,6 +4,7 @@ export const rankEvaluation = {
   "A": { from: 152, to: 178 },
   "A+": { from: 152, to: 203 },
   "S": { from: 187, to: 219 },
+  "S+": {from: 187, to: 252},
 }
 
 export const evaluationMap: {
