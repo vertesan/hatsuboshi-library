5d1a502d289a03c1a52f944621c9b24d8d33552e
vertesan <162897218+vertesan@users.noreply.github.com>
2024-10-07 16:44:36 +0000

    feat(locale): follow up on #18

--- app/routes/help.memories/route.tsx
+++ app/routes/help.memories/route.tsx
@@ -1,7 +1,9 @@
 import { TypographyStylesProvider } from "@mantine/core"
 import { MetaFunction } from "@remix-run/react"
+import { useTranslation } from "react-i18next"
 import { getLocalString } from "~/i18n"
 import MemoryMDX from "~/routes/help.memories/memories.mdx"
+import MemoryMDXzh_Hans from "~/routes/help.memories/memories_zh-Hans.mdx"
 
 export const meta: MetaFunction = () => {
   const title = getLocalString("meta-help-memories")
@@ -10,11 +12,22 @@ export const meta: MetaFunction = () => {
   ]
 }
 
-export default function About() {
+export default function HelpMemories() {
+  const { i18n } = useTranslation()
+  let mdx: JSX.Element
+  switch (i18n.language) {
+    case "zh-Hans":
+      mdx = <MemoryMDXzh_Hans />
+      break
+    default:
+      mdx = <MemoryMDX />
+      break
+  }
+
   return (
     <article className="p-4 max-w-[720px]">
       <TypographyStylesProvider>
-        <MemoryMDX />
+        {mdx}
       </TypographyStylesProvider>
     </article>
   )
